d71f07fe373a6fff7f010fe6c0ea54f8d23ea685
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2025-11-20 15:39:12 -0300

    opa

--- 02-Arquitectura/practica/routes/libroRoutes.js
+++ 02-Arquitectura/practica/routes/libroRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/libroController.js')
 const requireAuth = require('../middlewares/auth');
-const validate = require('../middlewares/validate');
+const validate = require('../middlewares/validate.js');
 const { 
     createLibroSchema, 
     updateLibroSchema 
-} = require('../validations/libroSchemas');
+} = require('../validations/libroSchemas.js');
 router.get('/', controller.getLibros)
 router.get('/:id', controller.getLibroById)
 router.post('/',requireAuth,validate(createLibroSchema),  controller.createLibro)
